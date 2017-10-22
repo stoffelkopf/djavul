34ceb1e43ae5dc8e6d5b9b3247189ff06bcb6db4
mewmew <rnd0x00@gmail.com>
2017-10-22 21:18:24 +0200

    diablo: add diablo_load_level_graphics

--- diablo/funcs.go
+++ diablo/funcs.go
@@ -9,6 +9,11 @@ package diablo
 //    printf("exit status: %d\n", status);
 //    exit(status);
 // }
+//
+// static void diablo_load_level_graphics() {
+//    void (*f)() = (void *)0x40A391;
+//    f();
+// }
 import "C"
 
 import (
@@ -24,4 +29,14 @@ func WinMain(inst, prev w32.HINSTANCE, args string, showCmd int) {
 	i := *(*C.HINSTANCE)(unsafe.Pointer(&inst))
 	p := *(*C.HINSTANCE)(unsafe.Pointer(&prev))
 	C.diablo_WinMain(i, p, C.CString(args), C.int(showCmd))
+}
+
+// LoadLevelGraphics loads the tile graphics of the active dungeon type.
+//
+// PSX ref: 0x80038930
+// PSX def: void LoadLvlGFX__Fv()
+//
+// ref: 0x40A391
+func LoadLevelGraphics() {
+	C.diablo_load_level_graphics()
 }
