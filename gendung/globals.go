ef80429accad68ff0e9a4455e5cae2c5c6da0f6e
mewmew <rnd0x00@gmail.com>
2017-10-23 15:09:58 +0200

    l1: add miniset globals for drlg_l1

--- gendung/globals.go
+++ gendung/globals.go
@@ -17,20 +17,24 @@ var (
 	//
 	// ref: 0x539608
 	TileIDMap = (*[40][40]uint8)(unsafe.Pointer(uintptr(0x539608)))
+
 	// PieceIDMap contains the piece IDs of each tile on the map.
 	//
 	// ref: 0x5A5BD8
 	PieceIDMap = (*[112][112]int32)(unsafe.Pointer(uintptr(0x5A5BD8)))
+
 	// TileDefs specifies the tile definitions of the active dungeon type; (e.g.
 	// levels/l1data/l1.til).
 	//
 	// ref: 0x5B70DC
 	TileDefs = (**til.Tile)(unsafe.Pointer(uintptr(0x5B70DC)))
+
 	// DType specifies the active dungeon type of the current game.
 	//
 	//
 	// ref: 0x5BB1ED
 	DType = (*DungeonType)(unsafe.Pointer(uintptr(0x5BB1ED)))
+
 	// DLvl specifies the active dungeon level of the current game.
 	//
 	// PSX ref: 0x8011C10C
@@ -38,6 +42,7 @@ var (
 	//
 	// ref: 0x5BB1EE
 	DLvl = (*int8)(unsafe.Pointer(uintptr(0x5BB1EE)))
+
 	// ArchNumMap contains the arch frame numbers of the map from the special
 	// tileset (e.g. "levels/l1data/l1s.cel"). Note, the special tileset of
 	// Tristram (i.e. "levels/towndata/towns.cel") contains trees rather than
--- l1/globals.go
+++ l1/globals.go
@@ -6,6 +6,117 @@ import "unsafe"
 
 // Global variables.
 var (
+	// Stair1 is a 4x4 miniset of tile IDs representing a staircase.
+	//
+	// Minisets specifies how to arrange tile IDs in order to form set areas of
+	// dungeons (e.g. staircases). Below follows a pseudo-code description of the
+	// miniset format.
+	//
+	//    // A miniset defines the set area of a dungeon in terms of before and
+	//    // after areas of tile IDs.
+	//    type miniset struct {
+	//       // Width of miniset area.
+	//       width uint8
+	//       // Height of miniset area.
+	//       height uint8
+	//       // Tile IDs before transformation.
+	//       before [width][height]uint8
+	//       // Tile IDs after transformation.
+	//       after [width][height]uint8
+	//    }
+	//
+	// ref: 0x479EC8
+	Stair1 = (*[34]uint8)(unsafe.Pointer(uintptr(0x479EC8)))
+
+	// Stair2 is a 4x4 miniset of tile IDs representing a staircase.
+	//
+	// Minisets specifies how to arrange tile IDs in order to form set areas of
+	// dungeons (e.g. staircases). Below follows a pseudo-code description of the
+	// miniset format.
+	//
+	//    // A miniset defines the set area of a dungeon in terms of before and
+	//    // after areas of tile IDs.
+	//    type miniset struct {
+	//       // Width of miniset area.
+	//       width uint8
+	//       // Height of miniset area.
+	//       height uint8
+	//       // Tile IDs before transformation.
+	//       before [width][height]uint8
+	//       // Tile IDs after transformation.
+	//       after [width][height]uint8
+	//    }
+	//
+	// ref: 0x479EEC
+	Stair2 = (*[34]uint8)(unsafe.Pointer(uintptr(0x479EEC)))
+
+	// Stair3 is a 4x3 miniset of tile IDs representing a staircase.
+	//
+	// Minisets specifies how to arrange tile IDs in order to form set areas of
+	// dungeons (e.g. staircases). Below follows a pseudo-code description of the
+	// miniset format.
+	//
+	//    // A miniset defines the set area of a dungeon in terms of before and
+	//    // after areas of tile IDs.
+	//    type miniset struct {
+	//       // Width of miniset area.
+	//       width uint8
+	//       // Height of miniset area.
+	//       height uint8
+	//       // Tile IDs before transformation.
+	//       before [width][height]uint8
+	//       // Tile IDs after transformation.
+	//       after [width][height]uint8
+	//    }
+	//
+	// ref: 0x479F10
+	Stair3 = (*[26]uint8)(unsafe.Pointer(uintptr(0x479F10)))
+
+	// Candlestick is a 2x2 miniset of tile IDs representing a candlestick.
+	//
+	// Minisets specifies how to arrange tile IDs in order to form set areas of
+	// dungeons (e.g. staircases). Below follows a pseudo-code description of the
+	// miniset format.
+	//
+	//    // A miniset defines the set area of a dungeon in terms of before and
+	//    // after areas of tile IDs.
+	//    type miniset struct {
+	//       // Width of miniset area.
+	//       width uint8
+	//       // Height of miniset area.
+	//       height uint8
+	//       // Tile IDs before transformation.
+	//       before [width][height]uint8
+	//       // Tile IDs after transformation.
+	//       after [width][height]uint8
+	//    }
+	//
+	// ref: 0x479F2C
+	Candlestick = (*[10]uint8)(unsafe.Pointer(uintptr(0x479F2C)))
+
+	// StairPoison is a 6x6 miniset of tile IDs representing a staircase down to
+	// the Poisoned Water Supply.
+	//
+	// Minisets specifies how to arrange tile IDs in order to form set areas of
+	// dungeons (e.g. staircases). Below follows a pseudo-code description of the
+	// miniset format.
+	//
+	//    // A miniset defines the set area of a dungeon in terms of before and
+	//    // after areas of tile IDs.
+	//    type miniset struct {
+	//       // Width of miniset area.
+	//       width uint8
+	//       // Height of miniset area.
+	//       height uint8
+	//       // Tile IDs before transformation.
+	//       before [width][height]uint8
+	//       // Tile IDs after transformation.
+	//       after [width][height]uint8
+	//    }
+	//
+	// ref: 0x479F38
+	StairPoison = (*[74]uint8)(unsafe.Pointer(uintptr(0x479F38)))
+
 	// FlagMap contains flags used for dungeon generation of the Cathedral.
 	//
 	// PSX ref: 0x8011C0D8
