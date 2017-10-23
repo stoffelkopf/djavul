00f54524f5b63710471ec337a991843baaa5e29b
mewmew <rnd0x00@gmail.com>
2017-10-23 16:58:15 +0200

    gendung: add SetXx, SetYy, ViewX, ViewY and TileIDMapBackup globals

--- gendung/globals.go
+++ gendung/globals.go
@@ -18,6 +18,14 @@ var (
 	// ref: 0x539608
 	TileIDMap = (*[40][40]uint8)(unsafe.Pointer(uintptr(0x539608)))
 
+	// TileIDMapBackup contains a backup of the tile IDs of the map.
+	//
+	// PSX ref: 0x800E52C4
+	// PSX def: unsigned char pdungeon[40][40]
+	//
+	// ref: 0x53CD50
+	TileIDMapBackup = (*[40][40]uint8)(unsafe.Pointer(uintptr(0x53CD50)))
+
 	// PieceIDMap contains the piece IDs of each tile on the map.
 	//
 	// ref: 0x5A5BD8
@@ -50,4 +58,36 @@ var (
 	//
 	// ref: 0x5C3008
 	ArchNumMap = (*[112][112]int8)(unsafe.Pointer(uintptr(0x5C3008)))
+
+	// SetXx specifies the active miniset x-coordinate of the map.
+	//
+	// PSX ref: 0x8011C0E4
+	// PSX def: int setpc_x
+	//
+	// ref: 0x5CF338
+	SetXx = (*int32)(unsafe.Pointer(uintptr(0x5CF338)))
+
+	// ViewX specifies the player viewpoint x-coordinate of the map.
+	//
+	// PSX ref: 0x8011C114
+	// PSX def: int ViewX
+	//
+	// ref: 0x5CF33C
+	ViewX = (*int32)(unsafe.Pointer(uintptr(0x5CF33C)))
+
+	// ViewY specifies the player viewpoint y-coordinate of the map.
+	//
+	// PSX ref: 0x8011C118
+	// PSX def: int ViewY
+	//
+	// ref: 0x5CF340
+	ViewY = (*int32)(unsafe.Pointer(uintptr(0x5CF340)))
+
+	// SetYy specifies the active miniset y-coordinate of the map.
+	//
+	// PSX ref: 0x8011C0E8
+	// PSX def: int setpc_y
+	//
+	// ref: 0x5CF344
+	SetYy = (*int32)(unsafe.Pointer(uintptr(0x5CF344)))
 )
