18cda28bda12aab171a8bc0446a248874980d6dc
mewmew <rnd0x00@gmail.com>
2017-10-31 21:52:39 +0100

    djavul: validate Poisoned Water Supply quest map

--- dlls/djavul/drlg.go
+++ dlls/djavul/drlg.go
@@ -23,16 +23,31 @@ import (
 func dumpL1Maps() {
 	dinit.Archives()
 	// Regular.
-	*gendung.DLvl = 1
-	*gendung.DType = gendung.Cathedral
+	//*gendung.DLvl = 1
+	//*gendung.DType = gendung.Cathedral
 
-	// Quest.
+	// Quest - The Butcher.
+	//for i := range quests.Quests {
+	//	quests.Quests[i].Active = false
+	//}
 	//*gendung.DLvl = 2
 	//*gendung.DType = gendung.Cathedral
 	//*gendung.IsQuestLevel = false
 	//*multi.MaxPlayers = 1
 	//quests.Quests[quests.TheButcher].Active = true
 	//quests.Quests[quests.TheButcher].DLvl = 2
+
+	// Quest - Poisoned Water Supply.
+	for i := range quests.Quests {
+		quests.Quests[i].Active = false
+	}
+	*gendung.DLvl = 2
+	*gendung.DType = gendung.Cathedral
+	*gendung.IsQuestLevel = false
+	*multi.MaxPlayers = 1
+	quests.Quests[quests.PoisonedWaterSupply].Active = true
+	quests.Quests[quests.PoisonedWaterSupply].DLvl = 2
+
 	diablo.LoadLevelGraphics()
 	for seed := int32(0); seed <= 0xFF; seed++ {
 		l1.CreateDungeon(seed, 0)
@@ -150,11 +165,27 @@ func checkL1Regular() error {
 	return nil
 }
 
-// checkL1Quest validates the implemenation of the dynamic random level
-// generation of Cathedral maps, for dungeon level 2 with the Butcher quest
-// active.
+// checkL1QuestTheButcher validates the implemenation of the dynamic random
+// level generation of Cathedral maps, for dungeon level 2 with the Butcher
+// quest active.
 func checkL1Quest() error {
+	if err := checkL1QuestTheButcher(); err != nil {
+		return errors.WithStack(err)
+	}
+	if err := checkL1QuestPoisonedWaterSupply(); err != nil {
+		return errors.WithStack(err)
+	}
+	return nil
+}
+
+// checkL1QuestTheButcher validates the implemenation of the dynamic random
+// level generation of Cathedral maps, for dungeon level 2 with the Butcher
+// quest active.
+func checkL1QuestTheButcher() error {
 	dinit.Archives()
+	for i := range quests.Quests {
+		quests.Quests[i].Active = false
+	}
 	*gendung.DLvl = 2
 	*gendung.DType = gendung.Cathedral
 	*gendung.IsQuestLevel = false
@@ -165,7 +196,7 @@ func checkL1Quest() error {
 	seed := int32(123)
 	l1.CreateDungeon(seed, 0)
 	if err := check(*gendung.TileIDMap, "tiles", seed, "659b95eec3e1c18d13b7f9932de108b88b356b9b"); err != nil {
-		path := fmt.Sprintf("testdata_quest/l1_tiles_%08X.bin", seed)
+		path := fmt.Sprintf("testdata_quest_the_butcher/l1_tiles_%08X.bin", seed)
 		f, e := os.Open(path)
 		if e != nil {
 			return errors.WithStack(e)
@@ -180,7 +211,7 @@ func checkL1Quest() error {
 		return errors.WithStack(err)
 	}
 	if err := check(*gendung.PieceIDMap, "pieces", seed, "15f2209ff5d066cfd568a1eab77e4328d08474e8"); err != nil {
-		path := fmt.Sprintf("testdata_quest/l1_pieces_%08X.bin", seed)
+		path := fmt.Sprintf("testdata_quest_the_butcher/l1_pieces_%08X.bin", seed)
 		f, e := os.Open(path)
 		if e != nil {
 			return errors.WithStack(e)
@@ -195,7 +226,7 @@ func checkL1Quest() error {
 		return errors.WithStack(err)
 	}
 	if err := check(*gendung.ArchNumMap, "arches", seed, "42941df3ada356ebf87ce2987d26a06c44da711a"); err != nil {
-		path := fmt.Sprintf("testdata_quest/l1_arches_%08X.bin", seed)
+		path := fmt.Sprintf("testdata_quest_the_butcher/l1_arches_%08X.bin", seed)
 		f, e := os.Open(path)
 		if e != nil {
 			return errors.WithStack(e)
@@ -210,7 +241,7 @@ func checkL1Quest() error {
 		return errors.WithStack(err)
 	}
 	if err := check(*gendung.TransparencyMap, "transparency", seed, "5f4e2e570b8a631d94fb3852c38ace0fa0397c7a"); err != nil {
-		path := fmt.Sprintf("testdata_quest/l1_transparency_%08X.bin", seed)
+		path := fmt.Sprintf("testdata_quest_the_butcher/l1_transparency_%08X.bin", seed)
 		f, e := os.Open(path)
 		if e != nil {
 			return errors.WithStack(e)
@@ -224,7 +255,88 @@ func checkL1Quest() error {
 		pretty.Println("want:", trans)
 		return errors.WithStack(err)
 	}
-	fmt.Println("PASS: quest")
+	fmt.Println("PASS: quest - The Butcher")
+	return nil
+}
+
+// checkL1QuestPoisonedWaterSupply validates the implemenation of the dynamic
+// random level generation of Cathedral maps, for dungeon level 2 with the
+// Poisoned Water Supply quest active.
+func checkL1QuestPoisonedWaterSupply() error {
+	dinit.Archives()
+	for i := range quests.Quests {
+		quests.Quests[i].Active = false
+	}
+	*gendung.DLvl = 2
+	*gendung.DType = gendung.Cathedral
+	*gendung.IsQuestLevel = false
+	*multi.MaxPlayers = 1
+	quests.Quests[quests.PoisonedWaterSupply].Active = true
+	quests.Quests[quests.PoisonedWaterSupply].DLvl = 2
+	diablo.LoadLevelGraphics()
+	seed := int32(123)
+	l1.CreateDungeon(seed, 0)
+	if err := check(*gendung.TileIDMap, "tiles", seed, "f4525775a47ef083d85c7faf5560b9808ce203ff"); err != nil {
+		path := fmt.Sprintf("testdata_quest_poisoned_water_supply/l1_tiles_%08X.bin", seed)
+		f, e := os.Open(path)
+		if e != nil {
+			return errors.WithStack(e)
+		}
+		defer f.Close()
+		var tiles [40][40]uint8
+		if err := binary.Read(f, binary.LittleEndian, &tiles); err != nil {
+			return errors.WithStack(err)
+		}
+		pretty.Println("got:", *gendung.ArchNumMap)
+		pretty.Println("want:", tiles)
+		return errors.WithStack(err)
+	}
+	if err := check(*gendung.PieceIDMap, "pieces", seed, "cb2f37c9d04a39ec22c4171c6e95c88a260364e3"); err != nil {
+		path := fmt.Sprintf("testdata_quest_poisoned_water_supply/l1_pieces_%08X.bin", seed)
+		f, e := os.Open(path)
+		if e != nil {
+			return errors.WithStack(e)
+		}
+		defer f.Close()
+		var pieces [112][112]int32
+		if err := binary.Read(f, binary.LittleEndian, &pieces); err != nil {
+			return errors.WithStack(err)
+		}
+		pretty.Println("got:", *gendung.ArchNumMap)
+		pretty.Println("want:", pieces)
+		return errors.WithStack(err)
+	}
+	if err := check(*gendung.ArchNumMap, "arches", seed, "87418c244b8123dbdb3439812a2e1d8af5032c21"); err != nil {
+		path := fmt.Sprintf("testdata_quest_poisoned_water_supply/l1_arches_%08X.bin", seed)
+		f, e := os.Open(path)
+		if e != nil {
+			return errors.WithStack(e)
+		}
+		defer f.Close()
+		var arches [112][112]int8
+		if err := binary.Read(f, binary.LittleEndian, &arches); err != nil {
+			return errors.WithStack(err)
+		}
+		pretty.Println("got:", *gendung.ArchNumMap)
+		pretty.Println("want:", arches)
+		return errors.WithStack(err)
+	}
+	if err := check(*gendung.TransparencyMap, "transparency", seed, "5d548a45afb50e56cd77847fd832822eee0b01e7"); err != nil {
+		path := fmt.Sprintf("testdata_quest_poisoned_water_supply/l1_transparency_%08X.bin", seed)
+		f, e := os.Open(path)
+		if e != nil {
+			return errors.WithStack(e)
+		}
+		defer f.Close()
+		var trans [112][112]int8
+		if err := binary.Read(f, binary.LittleEndian, &trans); err != nil {
+			return errors.WithStack(err)
+		}
+		pretty.Println("got:", *gendung.ArchNumMap)
+		pretty.Println("want:", trans)
+		return errors.WithStack(err)
+	}
+	fmt.Println("PASS: quest - Poisoned Water Supply")
 	return nil
 }
 
