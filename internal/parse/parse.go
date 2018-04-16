ad9a2b7bc00e70184bf4c977644cff039d8f0608
mewmew <rnd0x00@gmail.com>
2018-04-16 20:55:05 +0200

    internal/parse: validate checksum of diablo.exe before parsing

--- internal/parse/parse.go
+++ internal/parse/parse.go
@@ -3,6 +3,7 @@ package parse
 
 import (
 	"bytes"
+	"crypto/sha1"
 	"encoding/binary"
 	"fmt"
 	"io"
@@ -81,5 +82,34 @@ func init() {
 	buf, err = ioutil.ReadFile("diablo.exe")
 	if err != nil {
 		log.Fatalf("unable to read file; %v", errors.WithStack(err))
+	}
+	h := sha1.Sum(buf)
+	// The size in bytes of diablo.exe v1.09 and v1.09b are identical.
+	// Furthermore the offset to each function and global variable is identical,
+	// thus we can effortlessly support both versions.
+	switch hash := fmt.Sprintf("%040x", h[:]); hash {
+	case "accedfe32775d4a1984451309608c2a2d39ad406":
+		// v1.00dbg, not supported.
+		log.Fatalf("support for diablo.exe v1.00dbg not yet implemented (expected version 1.09 or 1.09b)")
+	case "9633d9bbf9a2dc3e88571cead6ac09d689c2abcf":
+		// v1.00, not supported.
+		log.Fatalf("support for diablo.exe v1.00 not yet implemented (expected version 1.09 or 1.09b)")
+	case "958e3503839a0798d544868d5fc3dff05c02e9fa":
+		// v1.04, not supported.
+		log.Fatalf("support for diablo.exe v1.04 not yet implemented (expected version 1.09 or 1.09b)")
+	case "d8af136407ea1c019c881f31a180a0bceda39226":
+		// v1.07, not supported.
+		log.Fatalf("support for diablo.exe v1.07 not yet implemented (expected version 1.09 or 1.09b)")
+	case "c2c111a1825c0410eec93f2f0dc872dd49f8c0db":
+		// v1.08, not supported.
+		log.Fatalf("support for diablo.exe v1.08 not yet implemented (expected version 1.09 or 1.09b)")
+	case "2119e1c8b818c27a06948979560cdeb4bec9ae65":
+		// v1.09, supported.
+	case "ebaee2acb462a0ae9c895a0e33079c94796cb0b6":
+		// v1.09b, supported.
+	case "e59538ac8de87063e5d3e921a0c5d629e8d54c4e":
+		// v1.09b (no CD), supported.
+	default:
+		log.Fatalf("support for unknown version of diablo.exe with SHA1 hashsum %q not yet implemented (expected version 1.09 or 1.09b)", hash)
 	}
 }
